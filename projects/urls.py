a1cc78877f4d4d460a78e1748416e34726f6d1c5
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-05-12 13:33:08 +0630

    Clean Up

--- projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .import views
-urlpatterns = [
-    path("", views.getProjects , name='projects'),
-    path('project/<str:id>', views.getProject, name="projectDetail")
-]
